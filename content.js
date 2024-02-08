eff16e33e95b13f5a57630c34f551c4318749c4c
Guilherme Duarte <guilhermesilva19860@gmail.com>
2024-02-07 22:27:08 -0300

    feat: Adiciona as funções iniciarLoop e pararLoop A função iniciarLoop executa filtraPorCriacao a cada intervalo definido, enquanto a função pararLoop interrompe a execução contínua iniciada por iniciarLoop.

--- content.js
+++ content.js
@@ -1,6 +1,38 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+let intervalId;
+
+function iniciarLoop(intervalo) {
+    intervalId = setInterval(() => filtraPorCriacao().then(() => document.querySelector('#tab_562').click()), intervalo);
+}
+
+function pararLoop() {
+    clearInterval(intervalId);
+}
+
+function waitForSelectChange(colSelect) {
+    return new Promise(resolve => colSelect.addEventListener('change', resolve, { once: true }));
+}
+
+function filtraPorCriacao() {
+    return new Promise(resolve => {
+        const colSelect = document.querySelector('select[name="post_filter[order][1][col]"]');
+        if (!colSelect) {
+            console.error('Select não encontrado');
+            return resolve();
+        }
+
+        colSelect.value = 'incidents.created_at';
+        waitForSelectChange(colSelect).then(resolve);
+        colSelect.dispatchEvent(new Event('change', { bubbles: true }));
+    });
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'executeScript') {
-        // Seu script aqui
+        const executeWithFilter = request.options.includes('script1');
+        filtraPorCriacao().then(() => iniciarLoop(request.interval));
         alert('Script executado!');
+
+    } else if (request.action === 'pararLoop') {
+        pararLoop();
     }
 });
--- popup.js
+++ popup.js
@@ -18,4 +18,11 @@ document.addEventListener('DOMContentLoaded', function () {
             chrome.tabs.sendMessage(tabs[0].id, { action: 'executeScript', options: selectedOptions, interval: loopInterval });
         });
     });
+
+    // Adicione o código para parar o looping
+    document.getElementById('pararLoop').addEventListener('click', function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'pararLoop' });
+        });
+    });
 });
