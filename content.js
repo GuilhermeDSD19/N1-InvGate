f4275976b3baf8604988b123711d6abd07c83bc0
GuilhermeDSD19 <guilhermesilva19860@gmail.com>
2024-09-14 01:51:48 -0300

    refactor(all): Refatoração do codigo e alteração das imagens de icones

--- content.js
+++ content.js
@@ -1,24 +1,6 @@
 let intervalId;
 const refreshButton = document.querySelector('#btn_refresh_requests');
 
-function filtraPorFila() {
-    // Criar a função
-}
-
-function removeLoading() {
-    const loading = document.querySelector('.content-loader')
-    if (loading) {
-        loading.style.display = 'none'
-        console.log("Apaguei")
-    } else {
-        console.log("Não apaguei")
-    }
-    /* btn.addEventListener('click', function () {
-        const contentDiv = document.querySelector('.content');
-        contentDiv.classList.remove('content--loading');
-    }); */
-}
-
 function iniciarLoop(intervalo, btnUpdate) {
     intervalId = setInterval(() => {
         btnUpdate.click();
@@ -30,9 +12,8 @@ function pararLoop() {
     clearInterval(intervalId);
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request) => {
     if (request.action === 'executeScript') {
-        // filtraPorFila() -- EM DESENVOLVIMENTO
         iniciarLoop(request.interval, refreshButton);
         alert('Script executado!');
     }
--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
-// Este código será executado após o DOM ser carregado completamente
 document.addEventListener('DOMContentLoaded', function () {
 
-    // Passa o intervalo de tempo e inicia o loop
     document.getElementById('executeScript').addEventListener('click', () => {
         const loopInterval = parseInt(document.getElementById('loopInterval').value);
 
@@ -10,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Finaliza o looping
     document.getElementById('pararLoop').addEventListener('click', () => {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { action: 'pararLoop' });
